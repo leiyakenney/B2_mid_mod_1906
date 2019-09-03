24635bf241d66c28d23b8aa9dabb72399e0bd94c
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-03 10:38:37 -0600

    completes user story 1

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -1,54 +1,37 @@
-# As a user,
-# When I visit '/books'
-# Then I see each book in the system including the book's:
-#   - title
-#   - number of pages
-#   - publication year
-#   - the name of each author that wrote the book
 require 'rails_helper'
 
 RSpec.describe "Books Index Page" do
   describe "When I visit the books index page" do
     before(:each) do
       @king = Author.create!(name: "Stephen King")
+      @it = @king.books.create!(title: "It", pages: 1138, year: 1986)
       @koontz = Author.create!(name: "Dean Koontz")
+      @shadow = @koontz.books.create!(title: "77 Shadow Street", pages: 556, year: 2011)
       @straub = Author.create!(name: "Peter Straub")
-
-      @talisman = Book.create!(title: "The Talisman", pages: 944, year: 2012)
-      @it = Book.create!(title: "It", pages: 1138, year: 1986)
-      @shadow = Book.create!(title: "77 Shadow Street", pages: 556, year: 2011)
-      @ghost = Book.create!(title: "Ghost Story", pages: 483, year: 1979)
+      @ghost = @straub.books.create!(title: "Ghost Story", pages: 483, year: 1979)
     end
 
     it "shows each book in the system with title, pages, publication year, and author" do
       visit "/books"
+      
+      within("#book-#{@it.id}") do
+        expect(page).to have_content(@it.title)
+        expect(page).to have_content(@it.pages)
+        expect(page).to have_content(@it.year)
+        expect(page).to have_content("Stephen King")
+      end
 
       within("#book-#{@shadow.id}") do
-        expect(page).to have_content(@shadow.title)
-        expect(page).to have_content(@shadow.pages)
-        expect(page).to have_content(@shadow.year)
-        expect(page).to have_content(@shadow.author.name)
+        expect(page).to have_content("Title: #{@shadow.title}")
+        expect(page).to have_content("Pages: #{@shadow.pages}")
+        expect(page).to have_content("Publication Year: #{@shadow.year}")
+        expect(page).to have_content("Author(s): #{@koontz.name}")
       end
 
       within("#book-#{@ghost.id}") do
         expect(page).to have_content(@ghost.title)
         expect(page).to have_content(@ghost.pages)
         expect(page).to have_content(@ghost.year)
-        expect(page).to have_content(@ghost.author.name)
-      end
-
-      within("#book-#{@it.id}") do
-        expect(page).to have_content(@it.title)
-        expect(page).to have_content(@it.pages)
-        expect(page).to have_content(@it.year)
-        expect(page).to have_content(@it.author.name)
-      end
-
-      within("#book-#{@talisman.id}") do
-        expect(page).to have_content(@talisman.title)
-        expect(page).to have_content(@talisman.pages)
-        expect(page).to have_content(@talisman.year)
-        expect(page).to have_content("Stephen King")
         expect(page).to have_content("Peter Straub")
       end
     end
