f62543b7d56d71ef33a30e188c9ad6f15dac2d40
Leiya Kenney <leiya.kenney@gmail.com>
2019-09-03 11:51:00 -0600

    Adds model test for Author Avg Pages

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Author, type: :model do
     it { should have_many :author_books}
     it { should have_many(:books).through(:author_books)}
   end
+
+  before(:each) do
+    @king = Author.create!(name: "Stephen King")
+    @it = Book.create!(title: "It", pages: 1138, year: 1986)
+    @shining = Book.create!(title: "The Shining", pages: 447, year: 1977)
+    @king.books << @it
+    @king.books << @shining
+  end
+
+  it "test average pages" do
+    expect(@king.avg_pages).to eq(792.5)
+  end
+
 end
